51eec85f9b7227a66aa3182063c99df1b723fbd0
Thiago Martins <thiago.martins@foton.la>
2025-04-26 10:41:01 -0300

    Updated: smoothScroll.js to compensate the fixed nav

--- assets/js/smoothScroll.js
+++ assets/js/smoothScroll.js
@@ -1,8 +1,16 @@
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
+
+    const target = document.querySelector(this.getAttribute("href"));
+    const navHeight = document.querySelector("#nav").offsetHeight;
+    const extra = 10; // tweak this for a few extra pixels
+    const y =
+      target.getBoundingClientRect().top +
+      window.pageYOffset -
+      navHeight -
+      extra;
+
+    window.scrollTo({ top: y, behavior: "smooth" });
   });
 });
