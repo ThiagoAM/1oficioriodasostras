ffae72816d88bef4a9ac54a341cad702837ecb97
Thiago Martins <thiago.martins@foton.la>
2026-02-11 18:04:24 -0300

    Fixed: Many layout issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
           ".info-card",
           ".service-card",
           ".pill-card",
+          ".useful-link-card",
           ".hours-panel",
           ".location-map-wrapper",
           ".about-image",
